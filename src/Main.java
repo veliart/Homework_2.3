b14ea23a83bc7d62143736193c43d8aa22f10e5d
Artur Vakhitov <arthurvakhitov@gmail.com>
2023-10-15 13:59:42 +0300

    Done homework 2.3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
+import service.ServiceStation;
+import transport.Bicycle;
+import transport.Car;
+import transport.Truck;
+
 public class Main {
     public static void main(String[] args) {
+        Bicycle bicycle = new Bicycle("Орлёнок", 2);
+        Car car = new Car("Москвич", 4);
+        Truck truck = new Truck("Камаз", 6);
+
+        ServiceStation serviceStation = new ServiceStation();
+        serviceStation.check(bicycle);
+        serviceStation.check(car);
+        serviceStation.check(truck);
     }
 }
